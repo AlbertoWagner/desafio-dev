4d71c54732de9ca4735d546f10604a9e35f4e683
alberto <alberto.wagner@m2atecnologia.com.br>
2021-08-21 18:07:23 -0300

    feat : new property balance & displays

--- store/models.py
+++ store/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from transactions.models import Transaction
+from transactions import choices
+
 
 class Stores(models.Model):
     name = models.CharField(max_length=120)
@@ -10,3 +13,27 @@ class Stores(models.Model):
         verbose_name = u'Store'
         verbose_name_plural = u'Stores'
         db_table = 'stores'
+
+    @property
+    def balance(self):
+        from django.db.models import Sum
+        entrada = Transaction.objects.filter(store=self, type__signal=choices.SUM).aggregate(
+            total=Sum('value')).get('total', 0) or 0
+        saida = Transaction.objects.filter(store=self, type__signal=choices.SUBTRACTION).aggregate(
+            total=Sum('value')).get('total', 0) or 0
+        return entrada - saida
+
+    @property
+    def balance_display(self):
+        return f'R$  {self.balance :.2f}'
+
+    @property
+    def transactions_count(self):
+        return Transaction.objects.filter(store=self).count()
+
+    @property
+    def get_display_status(self):
+        if self.balance > 0:
+            return 'breadcrumb-item table-success'
+        else:
+            return 'breadcrumb-item table-danger'
