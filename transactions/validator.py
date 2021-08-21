0886f9acd438f1915733e9642d586a9496e87219
alberto <alberto.wagner@m2atecnologia.com.br>
2021-08-21 15:24:32 -0300

    feat : new validate_file_extension

--- transactions/validator.py
+++ transactions/validator.py
@@ -28,3 +28,12 @@ def validaCpf(cpf):
         raise ValidationError('Cpf inválido: %s' % cpf)
     else:
         return True
+
+
+def validate_file_extension(value):
+    import os
+    from django.core.exceptions import ValidationError
+    ext = os.path.splitext(value.name)[1]
+    valid_extensions = ['.txt']
+    if not ext.lower() in valid_extensions:
+        raise ValidationError('Arquivo não suportado.')
