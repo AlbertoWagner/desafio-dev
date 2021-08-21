55ee3c07a688d21051eff97d073110c27f5fcd28
alberto <alberto.wagner@m2atecnologia.com.br>
2021-08-21 15:22:42 -0300

    feat : signals models transaction type create

--- transactions/apps.py
+++ transactions/apps.py
@@ -1,6 +1,23 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
 
-class TransactionsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'transactions'
+def create_types_all(sender, **kwargs):
+    from transactions.models import TransactionType
+    description = ['Débito', 'Boleto', 'Financiamento', 'Crédito', 'Recebimento Empréstimo', 'Vendas',
+                   'Recebimento TED', 'Recebimento DOC', 'Aluguel']
+    choices = [1, 2, 2, 1, 1, 1, 1, 1, 2]
+
+    for index in range(len(description)):
+        TransactionType.objects.update_or_create(type=index + 1, description=description[index], nature=choices[index],
+                                                 signal=choices[index])
+
+
+class TransactionConfig(AppConfig):
+    """App settings"""
+
+    name = "transactions"
+
+    def ready(self):
+        post_migrate.connect(create_types_all, sender=self)
+        import transactions.signals
