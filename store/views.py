2164c35f61971aa2d1df86735f2f853b3a601c0d
alberto <alberto.wagner@m2atecnologia.com.br>
2021-08-21 18:11:44 -0300

    feat : views store

--- store/views.py
+++ store/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+
+from django.views.generic import ListView
 
 # Create your views here.
+from store.models import Stores
+
+
+class StoresListView(ListView):
+    template_name = 'store/store_list.html'
+    model = Stores
+    paginate_by = 100
